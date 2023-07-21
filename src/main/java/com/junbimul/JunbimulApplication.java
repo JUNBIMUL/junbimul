af9a9e8daf2d48190656152faeeb0b08fb436272
김민균 <mingyun0324@gmail.com>
2023-07-21 21:06:04 +0900

    [fix] main 필요 없는 import 삭제

--- src/main/java/com/junbimul/JunbimulApplication.java
+++ src/main/java/com/junbimul/JunbimulApplication.java
@@ -1,6 +1,5 @@
 package com.junbimul;
 
-import com.junbimul.domain.Board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
