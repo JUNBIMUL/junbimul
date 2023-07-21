f19b64caf88cb9b9647e50b1bd3fc0ea4178f67a
김민균 <mingyun0324@gmail.com>
2023-07-21 20:23:12 +0900

    [refactor] git issue

--- src/main/java/com/junbimul/JunbimulApplication.java
+++ src/main/java/com/junbimul/JunbimulApplication.java
@@ -1,5 +1,6 @@
 package com.junbimul;
 
+import com.junbimul.domain.Board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
