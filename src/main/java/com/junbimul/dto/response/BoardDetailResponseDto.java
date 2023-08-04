2762bec172b2bdcdd891cb8f6ad8563b5f7237ed
김민균 <mingyun0324@gmail.com>
2023-08-04 12:19:03 +0900

    [feat] BoardDetailResponseDto 수정
    - id -> boardId

--- src/main/java/com/junbimul/dto/response/BoardDetailResponseDto.java
+++ src/main/java/com/junbimul/dto/response/BoardDetailResponseDto.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Builder
 @ToString
 public class BoardDetailResponseDto {
-    private Long id;
+    private Long boardId;
     private String title;
     private String content;
     private Long viewCnt;
